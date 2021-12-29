5bf9e6de254f2087e4a384d2b8ee42eb746c4b30
Doken Edgar <dokenedgarjr@gmail.com>
2021-12-29 14:09:57 +0100

    Updated purchased tickets to accept a movie parameter.

--- lib/views/tickets.dart
+++ lib/views/tickets.dart
@@ -1,3 +1,4 @@
+import 'package:cinema_app/view_model/movie_list_home_screen_view_model.dart';
 import 'package:cinema_app/widgets/purchased_ticket.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,18 @@ class Tickets extends StatefulWidget {
 }
 
 class _TicketsState extends State<Tickets> {
+
+  MoviesList moviesList = MoviesList();
+
+  @override
+  void initState() {
+    moviesList.addListener(() {
+      setState(() {});
+    });
+    moviesList.getMovies();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,29 +33,29 @@ class _TicketsState extends State<Tickets> {
               left: 24,
               right: 24,
             ),
-            child: Column(
+            child: moviesList.movies.isEmpty ? const Center(child: Text('Loading'),) : Column(
               
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Padding(
+              children:  [
+                const Padding(
                   padding: EdgeInsets.only(bottom: 8.0, top: 16),
                   child: Text(
                     'Today\'s Tickets',
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                 ),
-                PurchasedTicket(),
-                Padding(
+                PurchasedTicket(movie: moviesList.movies[0],),
+                const Padding(
                   padding: EdgeInsets.only(bottom: 8.0, top: 16),
                   child: Text(
                     'Upcoming Tickets',
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
                 ),
-                PurchasedTicket(),
-                PurchasedTicket(),
-                PurchasedTicket(),
-                PurchasedTicket(),
+                PurchasedTicket(movie: moviesList.movies[2],),
+                PurchasedTicket(movie: moviesList.movies[3],),
+                PurchasedTicket(movie: moviesList.movies[6],),
+                PurchasedTicket(movie: moviesList.movies[8],),
 
               ],
             ),
--- lib/widgets/purchased_ticket.dart
+++ lib/widgets/purchased_ticket.dart
@@ -1,10 +1,14 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cinema_app/models/movie.dart';
+import 'package:cinema_app/views/movie_detail.dart';
 import 'package:cinema_app/widgets/ticket_clip.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class PurchasedTicket extends StatefulWidget {
-  const PurchasedTicket({Key? key}) : super(key: key);
+  const PurchasedTicket({Key? key, required this.movie}) : super(key: key);
+
+  final Movie movie;
 
   @override
   _PurchasedTicketState createState() => _PurchasedTicketState();
@@ -15,75 +19,89 @@ class _PurchasedTicketState extends State<PurchasedTicket> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12.0),
-      child: ClipPath(
-        clipper: TicketClip(),
-        child: Container(
-          height: 150,
-          decoration: BoxDecoration(
-              color: Colors.grey.shade300,
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(12)),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CachedNetworkImage(
-                imageUrl:
-                    'https://m.media-amazon.com/images/M/MV5BZGExZTUzYWQtYWJjZi00OTI4LTk4OGYtNTA2YzcwMmNiZTMxXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UX1000_.jpg',
-                filterQuality: FilterQuality.medium,
-                height: 150,
-                width: 100,
-                fit: BoxFit.cover,
-                imageBuilder: (context, imageProvider) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey.shade400),
-                      borderRadius: BorderRadius.circular(12),
-                      image: DecorationImage(
-                        image: imageProvider,
-                        fit: BoxFit.contain,
+      child: InkWell(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MovieDetail(
+              movie: widget.movie,
+            ),
+          ),
+        ),
+        child: ClipPath(
+          clipper: TicketClip(),
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12)),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CachedNetworkImage(
+                  imageUrl: widget.movie.imageMedium,
+                  filterQuality: FilterQuality.medium,
+                  height: 150,
+                  width: 100,
+                  fit: BoxFit.cover,
+                  imageBuilder: (context, imageProvider) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey.shade400),
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.contain,
+                        ),
                       ),
+                    );
+                  },
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 34.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Text(
+                            widget.movie.title,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.start,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        const Text('21 December \'21'),
+                        const Text('04:00 pm'),
+                        RatingBar.builder(
+                          allowHalfRating: true,
+                          ignoreGestures: true,
+                          updateOnDrag: false,
+                          initialRating: num.parse(widget.movie.rating.toString()).toDouble(),
+                          itemSize: 20,
+                          itemBuilder: (context, _) {
+                            return const Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            );
+                          },
+                          onRatingUpdate: (value) {
+                            print('update.. $value');
+                          },
+                        ),
+                      ],
                     ),
-                  );
-                },
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 34.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const Text(
-                        'The Last Duel',
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.start,
-                        style:
-                            TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                      ),
-                      const Text('21 December \'21'),
-                      const Text('04:00 pm'),
-                      RatingBar.builder(
-                        allowHalfRating: true,
-                        ignoreGestures: true,
-                        updateOnDrag: false,
-                        initialRating: 3.5,
-                        itemSize: 20,
-                        itemBuilder: (context, _) {
-                          return const Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                          );
-                        },
-                        onRatingUpdate: (value) {
-                          print('update.. $value');
-                        },
-                      ),
-                    ],
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
